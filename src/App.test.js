22bd988fb7f41ac3ad521832c15309338523d962
Giorgio <giorgio.diprima@edu.turkuamk.fi>
2021-09-23 11:24:38 +0300

    First tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import UserDetails from './components/UserDetails'
+import ReactDOM from 'react-dom';
+import { MemoryRouter } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
+it('test no user found', () => {
+  render(
+    <MemoryRouter initialEntries={["/users/200"]}>
+      <UserDetails />
+    </MemoryRouter>
+  );
+  expect(screen.getByText('User not found!')).toBeInTheDocument();
+});
+// TODO: test when the data are loaded. After get, when state changes
